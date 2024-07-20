e0eb1d1e32d6b66918df7f5e65d2d1fefa0cd0a2
hirose <yukihio76@gmail.com>
2024-07-20 15:05:14 +0900

    不要なファイルの削除と移行

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  compiler: {
-    styledComponents: true,
-  },
+  reactStricMode: true,
+  compiler: (() => {
+    let compilerConfig = {
+      //styledComponentsの有効化
+      styledComponents: true,
+    };
+
+    if (process.env.NODE_ENV === "production") {
+      compilerConfig = {
+        ...compilerConfig,
+        //本番環境ではReact Testing Libraryで使用するdata-testid属性を削除
+        reactRemoveProperties: { properties: ["^data-testid$"] },
+      };
+    }
+    return compilerConfig;
+  })(),
 };
 
 export default nextConfig;
