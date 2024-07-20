f04ba12d27fb0db0d99c52f8789ee6a5a94b3691
hirose <yukihio76@gmail.com>
2024-07-20 14:05:25 +0900

    Next.jsのセットアップ

---
+++ next.config.js
@@ -0,0 +1,20 @@
+const nextConfig = {
+  reactStricMode: true,
+  compiler: (() => {
+    let compilerConfig = {
+      //styledComponentsの有効化
+      styledComponents: true,
+    };
+
+    if (process.env.NODE_ENV === "production") {
+      compilerConfig = {
+        ...compilerConfig,
+        //本番環境ではReact Testing Libraryで使用するdata-testid属性を削除
+        reactRemoveProperties: { properties: ["^data-testid$"] },
+      };
+    }
+    return compilerConfig;
+  })(),
+};
+
+module.exports = nextConfig;
